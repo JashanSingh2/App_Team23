b246ebe22d8060dc0e11fbda971aebca2c974887
jashan singh <jashanjatt229@gmail.com>
2025-01-13 15:06:31 +0530

    folder created

---
+++ App_Team23/DataController/RidesDataController.swift
@@ -0,0 +1,8 @@
+//
+//  RidesDataController.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 13/01/25.
+//
+
+import Foundation
---
+++ App_Team23/DataModel/DataModel.swift
@@ -0,0 +1,8 @@
+//
+//  DataModel.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 13/01/25.
+//
+
+import Foundation
