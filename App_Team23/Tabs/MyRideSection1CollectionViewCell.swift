5df63544b8dc46cd2fe0c9dfb73fdde3ba5ea6b0
Firdosh-263 <firdoshalam263@gmail.com>
2025-01-15 16:20:21 +0530

    firdosh first commit

---
+++ App_Team23/Tabs/MyRideSection1CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyRideSection1CollectionViewCell.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 15/01/25.
+//
+
+import UIKit
+
+class MyRideSection1CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var sourceLocationLabel: UILabel!
+    @IBOutlet weak var destinationLocationLabel: UILabel!
+    @IBOutlet weak var sourceTimeLabel: UILabel!
+    @IBOutlet weak var rideFareLabel: UILabel!
+    
+}
