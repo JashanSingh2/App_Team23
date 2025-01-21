640f64f54a2671f1f89b6d3ee0dd5278f5daca68
Firdosh-263 <firdoshalam263@gmail.com>
2025-01-21 16:01:45 +0530

    done

---
+++ App_Team23/myRides/SeatBookingViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SeatBookingViewController.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 21/01/25.
+//
+
+import UIKit
+
+class SeatBookingViewController: UIViewController {
+    @IBOutlet weak var routeView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        routeView.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
