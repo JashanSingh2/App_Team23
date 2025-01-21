3c4c96dc89654856eb798c055fd426713b655e26
jashan singh <jashanjatt229@gmail.com>
2025-01-21 11:54:04 +0530

    map view added

--- App_Team23/lestGo/SearchViewController.swift
+++ App_Team23/lestGo/SearchViewController.swift
@@ -9,15 +9,35 @@ import UIKit
 import MapKit
 
 
-
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet var modalVCView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.showsUserLocation = true
+        //mapView.showsUserLocation = true
+        
+        //modalVCView.isHidden = true
+        
+        let modalVC = ModelContentViewController()
+        modalVC.modalPresentationStyle = .pageSheet
+        
+        if let sheet = modalVC.sheetPresentationController {
+            sheet.detents = [.medium(),.large()]
+            
+        }
+        //modalVC.view.backgroundColor = .white
+        modalVC.view.addSubview(modalVCView)
+        //modalVC.modalOuterView.isHidden = false
+        
+        present(modalVC, animated: true)
+        
+        
+        
+        
+        
         //mapView.layer.
         // Do any additional setup after loading the view.
     }
