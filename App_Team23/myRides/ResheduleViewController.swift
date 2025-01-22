ddc0c568e3fbabd655e1843bb808e7faacb4f9b8
Firdosh-263 <firdoshalam263@gmail.com>
2025-01-22 11:13:02 +0530

    shadow added

---
+++ App_Team23/myRides/ResheduleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResheduleViewController.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 22/01/25.
+//
+
+import UIKit
+
+class ResheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
