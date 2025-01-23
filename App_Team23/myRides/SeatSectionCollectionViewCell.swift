07da51abb62b3c1855c4ac853430b3f2409cd1fd
Firdosh-263 <firdoshalam263@gmail.com>
2025-01-23 12:30:18 +0530

    Seat boooking layout  done

---
+++ App_Team23/myRides/SeatSectionCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  SeatSectionCollectionViewCell.swift
+//  App_Team23
+//
+//  Created by Batch - 1 on 23/01/25.
+//
+
+import UIKit
+
+class SeatSectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var seatButton: UIButton!
+    
+    
+    func updateSeatButton(with indexPath: IndexPath){
+        seatButton.setTitle("\((indexPath.row+1) * (indexPath.section+1))", for: .normal)
+        
+    }
+    
+}
--- App_Team23/myRides/myRide ViewControllers/SeatBookingViewController.swift
+++ App_Team23/myRides/myRide ViewControllers/SeatBookingViewController.swift
@@ -7,18 +7,62 @@
 
 import UIKit
 
-class SeatBookingViewController: UIViewController {
+class SeatBookingViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
     @IBOutlet weak var routeView: UIView!
+    @IBOutlet weak var seatCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
         routeView.layer.cornerRadius = 10
+        
+        let nib = UINib(nibName: "SeatSectionCell", bundle: nil)
+        seatCollectionView.register(nib, forCellWithReuseIdentifier: "SeatSectionCell")
+        
+        seatCollectionView.setCollectionViewLayout(generateSeatLayout(), animated: true)
+        
+        seatCollectionView.delegate = self
+        seatCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
-
-    /*
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SeatSectionCell", for: indexPath) as! SeatSectionCollectionViewCell
+        
+        cell.updateSeatButton(with: indexPath)
+        return cell
+    }
+    
+    func generateSeatLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let section : NSCollectionLayoutSection
+            
+            let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(30), heightDimension: .absolute(30))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .absolute(200), heightDimension: .absolute(30))
+            
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 10)
+            section = NSCollectionLayoutSection(group: group)
+            
+            return section
+        }
+        return layout
+    }
+    
+     
+    
+    
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -26,6 +70,5 @@ class SeatBookingViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
