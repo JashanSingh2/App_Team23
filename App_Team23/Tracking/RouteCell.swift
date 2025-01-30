f0229913b716ea7a7d8460bd755d38e4fd6db5f5
jashan singh <jashanjatt229@gmail.com>
2025-01-30 12:56:27 +0530

    test

---
+++ App_Team23/Tracking/RouteCell.swift
@@ -0,0 +1,33 @@
+//
+//  RouteCell.swift
+//  Ride Tracking
+//
+//  Created by Batch - 2 on 22/01/25.
+//
+
+import UIKit
+
+class RouteCell: UITableViewCell {
+
+    
+    @IBOutlet weak var stopMarker: UIView!
+    
+    @IBOutlet weak var stopNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var stopTImeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        //stopMarker.layer.cornerRadius = stopMarker.frame.height / 6
+        stopMarker.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
